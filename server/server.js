01206816fa921ded3817d7d4849f87f8fa82c6f5
Hashicy <harshita.joshi.1505@gmail.com>
2025-11-13 12:15:43 +0530

    Savepoint 1: Working frontend-backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = 5173;
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
 });
